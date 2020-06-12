8a7f16138e0a648834c8297cf9a1dd076e04041a
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-12 11:11:52 -0700

    provide and output return of my range of numbers from 0 to the given number and it will replace 3's with the appropriate greeting

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 // Business Logic ------------- //
 
 function robotSpeak(stringToNum) {
+let rangeArray = [];
 for (let index =0; index <= stringToNum; index+=1)  {
-  index;
+  if (index === 3) {
+    rangeArray.push("Won't you be my neighbor?");
+  } else {
+    rangeArray.push(index);
+  }
 }
+  return rangeArray;
 }
 
 
@@ -18,6 +24,6 @@ $(document).ready(function()  {
     event.preventDefault();
     let stringToNum = (parseInt($("#stringInput").val()));
     let output = robotSpeak(stringToNum);
-    $("#output").text(robotSpeak);
+    $("#output").text(output);
   })
 })
